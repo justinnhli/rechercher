8eb388ad108a3c071483104198ab9877ceaed8e7
justinnhli <justinnhli@gmail.com>
2015-02-21 21:37:17 -0500

    print test output in more legible form

--- test.py
+++ test.py
@@ -6,12 +6,15 @@ from domains import GridWorld
 def main():
     problem = SearchProblem(
         GridWorld(20, 20),
-        GridWorld.get_state_class()(x=0, y=0),
+        GridWorld.get_state(x=0, y=0),
         (lambda state: state.x == 19 and state.y == 15),
-        heuristic=(lambda state: abs(19 - state.x) + abs(15 - state.y)))
+        heuristic=GridWorld.heuristic(GridWorld.get_state(x=19, y=15)))
     solution = search(problem, UNIFORM_COST_SEARCH)
     if solution:
-        print(solution.path)
+        print(solution.path[0])
+        for action, state in zip(solution.actions, solution.path[1:]):
+            print("  action: {}".format(action))
+            print(state)
 
 if __name__ == "__main__":
     main()
